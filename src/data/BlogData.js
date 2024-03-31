ff5bd42edf4ddc9e2745f8f11a0e6d6cab5faf2e
kkbp27 <kotchakon391@gmail.com>
2024-03-31 20:15:08 +0700

    create portfolio
    create portfolio

---
+++ src/data/BlogData.js
@@ -0,0 +1,13 @@
+// // blog data
+
+// export const Blogs = [
+//     {
+//         id:1,
+//         name:"Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations",
+//         tags:["react","gsap","styled-components"],
+//         date:"13 May, 2021",
+//         imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        
+//     },
+    
+// ] 
