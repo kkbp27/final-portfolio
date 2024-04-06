a7342ad1ddc5a2621a9172d9c630b2f8eb09768b
kkbp27 <kotchakon391@gmail.com>
2024-04-07 02:39:01 +0700

    FN2
    FN2

--- src/data/EducationData.js
+++ src/data/EducationData.js
@@ -4,14 +4,14 @@ export const Education = [
         id: 1,
         name: "Resume",
         description: "If you want to see my resume.",
-        tags: ["This is my resume"],
+        tags: ["Click to visit"],
         demo: "https://drive.google.com/file/d/1p_MsmDOtD92fAG92z77gefx3jjnYPEjX/view?usp=sharing",
     },
     {
         id: 2,
         name: "Transcript",
-        description: "If you want to see my Transcript.",
-        tags: ["This is my Transcript"],
+        description: "King Mongkut's University of Technology North Bangkok.",
+        tags: ["Bachelor Degree , Faculty, College of Industrial Technology in Electronics Computer Technology."],
         demo: "https://drive.google.com/file/d/1FJ88caLMNy_K1gciDpsImRS5TJObDlXC/view?usp=sharing/",
 
     },
